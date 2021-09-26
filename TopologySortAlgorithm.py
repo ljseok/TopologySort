fced1140613ae4cdd29fb8024938dca75e1fe92f
ccccljs <cho38suk@hamail.net>
2021-09-26 14:23:03 +0900

    위상 정렬 알고리즘

--- TopologySortAlgorithm.py
+++ TopologySortAlgorithm.py
@@ -16,3 +16,18 @@ def topology_sort(): # 위상정렬 함수 정의
     for i in range(1, v+1):
         if indgree[i] == 0: # 진입차수가 0인 노드를
             q.append(i) # 큐에 삽입
+
+    while q: # 큐가 빌 때까지 반복
+        now = q.popleft() # 큐에서 원소를 꺼낸다
+        result.append(now)
+
+        for i in graph[now]: # 해당 원소와 연결된 노드중에서
+            indgree[i] -= 1 # 진입차수를 1만큼 뺀다
+
+            if indgree[i] == 0: # 새롭게 진입차수가 0이되는 노드를
+                q.append(i) # 큐에 삽입
+
+    for i in result:
+        print(i, end=' ')
+
+topology_sort()
